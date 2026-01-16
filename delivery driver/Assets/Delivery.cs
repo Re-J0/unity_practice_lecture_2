1434328e6cc3d8d4fa92faba21f4e0e2da135add
Re:J0 <wogus9728@gmail.com>
2026-01-16 14:22:43 +0900

    Bool 사용하기
    1. Bool 변수를 사용하여 물품 픽업 시 해당 변수를 true 로 만들어 물품을 가지고 있다 설정
    2. 고객에게 배달 시 물품이 없으면 배달 완료가 안되게 && and 연산으로 지정
    3. 배달 완료 시 물품 여부를 지움으로 서 정상동작하게 함

--- delivery driver/Assets/Delivery.cs
+++ delivery driver/Assets/Delivery.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("마 자신있나");
@@ -13,11 +15,13 @@ public class Delivery : MonoBehaviour
         if(other.tag == "Package")
         {
             Debug.Log("패키지 픽업됨.");
+            hasPackage = true;
         }
 
-        else if (other.tag == "Customer")
+        else if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("고객 배달 완료.");
+            hasPackage = false;
         }
     }
 }
