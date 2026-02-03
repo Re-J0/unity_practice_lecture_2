93f000467b7b06b01d041e043376d4a33f92b82f
Re:J0 <wogus9728@gmail.com>
2026-02-03 20:10:43 +0900

    부스트 & 범프
    1. 부스트를 담당하는 게임 오브젝트 생성
    2. 부스트 오브젝트에 2d 콜라이더 적용
    3. 부스트 오브젝트를 트리거로 적용
    4. 부스트 오브젝트에 부스트  태그 적용
    5. 드라이버 스크립트에서 부스트 오브젝트를 지났을 때의 속도와 어딘가에 부딪혔을 때의 속도를 나타낼 직렬화 필드 생성
    6. 부스트 오브젝트를 지나가면 무브 스피드를 부스트 스피드로 변경
    7. 어딘가에 부딪히면 무브 스피드를 슬로우 스피드로 변경

--- delivery driver/Assets/Driver.cs
+++ delivery driver/Assets/Driver.cs
@@ -1,17 +1,13 @@
+using System;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
 
     [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
 
     // Update is called once per frame
     void Update()
@@ -21,4 +17,16 @@ public class Driver : MonoBehaviour
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0,moveAmount,0);
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        moveSpeed = slowSpeed;
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Boost")
+            moveSpeed = boostSpeed;
+    }
+
+    
 }
